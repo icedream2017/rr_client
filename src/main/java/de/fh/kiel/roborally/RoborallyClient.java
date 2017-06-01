7e46cf667265e1ba833ff55903bfbd9cbf38f2cc
jiayi.zhao <jiayi.zhao@student.fh-kiel.de>
2017-06-01 12:37:09 +0200

    Initial commit

---
+++ src/main/java/de/fh/kiel/roborally/RoborallyClient.java
@@ -0,0 +1,11 @@
+package de.fh.kiel.roborally;
+
+/**
+ * Created by Andreas on 2017/6/1.
+ */
+public class RoborallyClient {
+
+    public static void main(String[] args) {
+        System.out.println("This is Client of Roborally.");
+    }
+}
